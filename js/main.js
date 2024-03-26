ec398d7112829a0f5b7ccab9a37fd1bfed6d8de6
Aleeidk <148299469+Aleeidk@users.noreply.github.com>
2024-03-25 23:46:38 -0600

    financiamiento

---
+++ js/main.js
@@ -0,0 +1,58 @@
+let mostrador = document.getElementById("mostrador");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccionada = document.getElementById("img");
+let modeloSeleccionado = document.getElementById("modelo");
+let descripSeleccionada = document.getElementById("descripcion");
+let precioSeleccionado = document.getElementById("precio");
+
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () =>{
+    search.classList.toggle('active');
+}
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    menu.classList.toggle('active');
+    search.classList.remove('active')
+}
+
+//header
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+function cargar(item){
+    quitarBordes();
+    mostrador.style.width = "60%";
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+    item.style.border = "2px solid red";
+
+    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
+
+    modeloSeleccionado.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
+
+    descripSeleccionada.innerHTML = "Descripción del modelo ";
+
+    precioSeleccionado.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
+
+
+}
+function cerrar(){
+    mostrador.style.width = "100%";
+    seleccion.style.width = "0%";
+    seleccion.style.opacity = "0";
+    quitarBordes();
+}
+function quitarBordes(){
+    var items = document.getElementsByClassName("item");
+    for(i=0;i <items.length; i++){
+        items[i].style.border = "none";
+    }
+}
+
